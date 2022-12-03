9fe376bd0bddda6e3348d01b5f317009cdddbcaf
Nour Ahmed <sim.nour.ahmed@alexu.edu.eg>
2022-12-03 21:08:34 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController? mycontroller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    mycontroller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      TextField(
+        controller: mycontroller,
+      )
+    ])));
+  }
+}
